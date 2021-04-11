8c7f19c625241af580f88b42d94961bedefcd958
Ivan Deryabin <ivan.deryabin@etu.univ-grenoble-alpes.fr>
2021-04-11 12:24:08 +0200

    Javadoc

--- DevopsProject/src/main/java/main/App.java
+++ DevopsProject/src/main/java/main/App.java
@@ -2,6 +2,7 @@ package main;
 
 /**
  * Hello world!
+ * Le test de JavaDoc
  *
  */
 public class App 
@@ -9,5 +10,9 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+    }
+    
+    public static int add(int a,int b) {
+    	return a+b;
     }
 }
--- DevopsProject/src/test/java/main/AppTest.java
+++ DevopsProject/src/test/java/main/AppTest.java
@@ -1,6 +1,8 @@
 package main;
 
 import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotEquals;
 
 import org.junit.Test;
 
@@ -16,5 +18,12 @@ public class AppTest
     public void shouldAnswerWithTrue()
     {
         assertTrue( true );
+    }
+    
+    @Test
+    public void addition()
+    {
+    	assertEquals("Test1 1+2 = 3",3,App.add(1, 2));
+    	assertNotEquals("Test1 1+2 != 4",4,App.add(1, 2));
     }
 }
